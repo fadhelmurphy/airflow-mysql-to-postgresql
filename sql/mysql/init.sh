6ebdd52d787d44dfc1cebe0cddef5cad4bd927c6
Fadhel ngepush ⚡ <fadhelijlalfalah@gmail.com>
2024-01-06 17:18:24 +0700

    fix: image mysql

--- sql/mysql/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Menjalankan file SQL pertama
-echo "Menjalankan sakila-schema.sql..."
-mysql -u root -proot < /docker-entrypoint-initdb.d/sakila-schema.sql
-
-# Menjalankan file SQL kedua
-echo "Menjalankan sakila-data.sql..."
-mysql -u root -proot < /docker-entrypoint-initdb.d/sakila-data.sql
